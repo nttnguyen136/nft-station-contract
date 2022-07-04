c55cdd244e3c4eff95b75f85853f36e1963f5423
nttnguyen136 <nttnguyen136@gmail.com>
2022-07-04 22:43:31 +0700

    implement instantiate minter

--- contracts/nft-station-minter/src/contract.rs
+++ contracts/nft-station-minter/src/contract.rs
@@ -1,13 +1,20 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::Empty;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-use cw2::set_contract_version;
+use cosmwasm_std::{to_binary, Empty, SubMsg};
 
+use crate::state::{Config, CONFIG, MINTABLE_NUM_TOKENS, MINTABLE_TOKEN_IDS};
+use cosmwasm_std::{
+    Binary, Deps, DepsMut, Env, MessageInfo, ReplyOn, Response, StdResult, WasmMsg,
+};
+use cw2::set_contract_version;
+use url::Url;
+
+use crate::error::ContractError;
+use crate::msg::InstantiateMsg;
 use crate::Extension;
 
 // CW721
-pub use cw721_base::{ContractError, InstantiateMsg, MintMsg, MinterResponse, QueryMsg};
+pub use cw721_base::{InstantiateMsg as CW721InstantiateMsg, MintMsg, MinterResponse, QueryMsg};
 pub type Cw721MetadataContract<'a> = cw721_base::Cw721Contract<'a, Extension, Empty>;
 pub type ExecuteMsg = cw721_base::ExecuteMsg<Extension>;
 
@@ -15,15 +22,98 @@ pub type ExecuteMsg = cw721_base::ExecuteMsg<Extension>;
 const CONTRACT_NAME: &str = "crates.io:nft-station-minter";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
+// governance parameters
+pub(crate) const MAX_TOKEN_LIMIT: u32 = 10000;
+pub(crate) const MAX_TOKEN_PER_BATCH_LIMIT: u32 = 20;
+pub(crate) const INSTANTIATE_CW721_REPLY_ID: u64 = 1;
+
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     env: Env,
     info: MessageInfo,
     msg: InstantiateMsg,
-) -> StdResult<Response> {
+) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-    Cw721MetadataContract::default().instantiate(deps, env, info, msg)
+    // Check the number of tokens is more than zero and less than the max limit
+    if msg.num_tokens == 0 || msg.num_tokens > MAX_TOKEN_LIMIT {
+        return Err(ContractError::InvalidNumTokens {
+            min: 1,
+            max: MAX_TOKEN_LIMIT,
+        });
+    }
+
+    // Check the number of tokens per batch is more than zero and less than the max limit
+    if msg.max_tokens_per_batch_mint == 0
+        || msg.max_tokens_per_batch_mint > MAX_TOKEN_PER_BATCH_LIMIT
+    {
+        return Err(ContractError::InvalidMaxTokensPerBatchMint {
+            min: 1,
+            max: MAX_TOKEN_PER_BATCH_LIMIT,
+        });
+    }
+
+    // Check the number of tokens per batch is more than zero and less than the max limit
+    if msg.max_tokens_per_batch_transfer == 0
+        || msg.max_tokens_per_batch_transfer > MAX_TOKEN_PER_BATCH_LIMIT
+    {
+        return Err(ContractError::InvalidMaxTokensPerBatchTransfer {
+            min: 1,
+            max: MAX_TOKEN_PER_BATCH_LIMIT,
+        });
+    }
+
+    // Check that base_token_uri is a valid IPFS uri
+    let parsed_token_uri = Url::parse(&msg.base_token_uri)?;
+    if parsed_token_uri.scheme() != "ipfs" {
+        return Err(ContractError::InvalidBaseTokenURI {});
+    }
+
+    let config = Config {
+        owner: info.sender.clone(),
+        cw721_code_id: msg.cw721_code_id,
+        cw721_address: None,
+        name: msg.name.clone(),
+        symbol: msg.symbol.clone(),
+        base_token_uri: msg.base_token_uri.clone(),
+        max_tokens: msg.num_tokens,
+        max_tokens_per_batch_mint: msg.max_tokens_per_batch_mint,
+        max_tokens_per_batch_transfer: msg.max_tokens_per_batch_transfer,
+        royalty_percentage: msg.royalty_percentage,
+        royalty_payment_address: msg.royalty_payment_address,
+    };
+    CONFIG.save(deps.storage, &config)?;
+    MINTABLE_NUM_TOKENS.save(deps.storage, &msg.num_tokens)?;
+    let sub_msgs: Vec<SubMsg> = vec![SubMsg {
+        id: INSTANTIATE_CW721_REPLY_ID,
+        msg: WasmMsg::Instantiate {
+            admin: Some(info.sender.to_string()),
+            code_id: msg.cw721_code_id,
+            msg: to_binary(&CW721InstantiateMsg {
+                name: msg.name,
+                symbol: msg.symbol,
+                minter: env.contract.address.to_string(),
+            })?,
+            funds: vec![],
+            label: String::from("Check CW721"),
+        }
+        .into(),
+        gas_limit: None,
+        reply_on: ReplyOn::Success,
+    }];
+
+    // Save mintable token ids map
+    for token_id in 1..=msg.num_tokens {
+        MINTABLE_TOKEN_IDS.save(deps.storage, token_id, &true)?;
+    }
+
+    Ok(Response::new()
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender)
+        .add_attribute("contract_name", CONTRACT_NAME)
+        .add_attribute("contract_version", CONTRACT_VERSION)
+        .add_submessages(sub_msgs))
+    // Cw721MetadataContract::default().instantiate(deps, env, info, msg)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -33,7 +123,8 @@ pub fn execute(
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    Cw721MetadataContract::default().execute(deps, env, info, msg)
+    Ok(Response::new())
+    // Cw721MetadataContract::default().execute(deps, env, info, msg)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
--- contracts/nft-station-minter/src/msg.rs
+++ contracts/nft-station-minter/src/msg.rs
@@ -3,7 +3,24 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
+    /// base_token_uri of NFTs
+    pub base_token_uri: String,
+    /// number token of NFTs
+    pub num_tokens: u32,
+    /// max number token of NFTs can be minted a batch
+    pub max_tokens_per_batch_mint: u32,
+    /// max number token of NFTs can be transferred a batch
+    pub max_tokens_per_batch_transfer: u32,
+    /// code id of cw721 was deploy before
+    pub cw721_code_id: u64,
+    /// name of NFTs
+    pub name: String,
+    /// symbol of NFTs
+    pub symbol: String,
+    /// royalty percentage can be received
+    pub royalty_percentage: Option<u64>,
+    /// royalty addresses
+    pub royalty_payment_address: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
