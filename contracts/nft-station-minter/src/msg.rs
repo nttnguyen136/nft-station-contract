5de31447d4a22d055e71b8aef4439844f7eb4f67
nttnguyen136 <nttnguyen136@gmail.com>
2022-07-05 22:07:05 +0700

    implements minter contract

--- contracts/nft-station-minter/src/msg.rs
+++ contracts/nft-station-minter/src/msg.rs
@@ -1,5 +1,9 @@
+use cosmwasm_std::{Uint128, Addr};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use cw721_base::msg::QueryMsg as CW721QueryMsg;
+
+use crate::Extension;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
@@ -45,13 +49,136 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    /// Return config info set in Instantiate
+    GetConfig {},
+    ///Return royalty info include Royalty address and the royalty fee must be pay.
+    RoyaltyInfo { sale_price: Uint128 },
+    /// Return the owner of the given token, error if token does not exist
+    /// Return type: OwnerOfResponse
+    OwnerOf {
+        token_id: String,
+        /// unset or false will filter out expired approvals, you must set to true to see them
+        include_expired: Option<bool>,
+    },
+    /// List all operators that can access all of the owner's tokens.
+    /// Return type: `OperatorsResponse`
+    AllOperators {
+        owner: String,
+        /// unset or false will filter out expired approvals, you must set to true to see them
+        include_expired: Option<bool>,
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+    /// Total number of tokens issued
+    NumTokens {},
+
+    /// With MetaData Extension.
+    /// Returns top-level metadata about the contract: `ContractInfoResponse`
+    ContractInfo {},
+    /// With MetaData Extension.
+    /// Returns metadata about one particular token, based on *ERC721 Metadata JSON Schema*
+    /// but directly from the contract: `NftInfoResponse`
+    NftInfo { token_id: String },
+    /// With MetaData Extension.
+    /// Returns the result of both `NftInfo` and `OwnerOf` as one query as an optimization
+    /// for clients: `AllNftInfo`
+    AllNftInfo {
+        token_id: String,
+        /// unset or false will filter out expired approvals, you must set to true to see them
+        include_expired: Option<bool>,
+    },
+
+    /// With Enumerable extension.
+    /// Returns all tokens owned by the given address, [] if unset.
+    /// Return type: TokensResponse.
+    Tokens {
+        owner: String,
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+    /// With Enumerable extension.
+    /// Requires pagination. Lists all token_ids controlled by the contract.
+    /// Return type: TokensResponse.
+    AllTokens {
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+}
+
+
+impl From<QueryMsg> for CW721QueryMsg {
+    fn from(msg: QueryMsg) -> CW721QueryMsg {
+        match msg {
+            QueryMsg::OwnerOf {
+                token_id,
+                include_expired,
+            } => CW721QueryMsg::OwnerOf {
+                token_id,
+                include_expired,
+            },
+            QueryMsg::AllOperators {
+                owner,
+                include_expired,
+                start_after,
+                limit,
+            } => CW721QueryMsg::AllOperators {
+                owner,
+                include_expired,
+                start_after,
+                limit,
+            },
+            QueryMsg::NumTokens {} => CW721QueryMsg::NumTokens {},
+            QueryMsg::ContractInfo {} => CW721QueryMsg::ContractInfo {},
+            QueryMsg::NftInfo { token_id } => CW721QueryMsg::NftInfo { token_id },
+            QueryMsg::AllNftInfo {
+                token_id,
+                include_expired,
+            } => CW721QueryMsg::AllNftInfo {
+                token_id,
+                include_expired,
+            },
+            QueryMsg::Tokens {
+                owner,
+                start_after,
+                limit,
+            } => CW721QueryMsg::Tokens {
+                owner,
+                start_after,
+                limit,
+            },
+            QueryMsg::AllTokens { start_after, limit } => {
+                CW721QueryMsg::AllTokens { start_after, limit }
+            }
+            _ => panic!("cannot covert {:?} to CW721QueryMsg", msg),
+        }
+    }
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct ConfigResponse {
+    pub owner: Addr,
+    pub cw721_code_id: u64,
+    pub cw721_address: Option<Addr>,
+    pub max_tokens: u32,
+    pub max_tokens_per_mint: u32,
+    pub max_tokens_per_batch_transfer: u32,
+    pub name: String,
+    pub symbol: String,
+    pub base_token_uri: String,
+    pub extension: Extension,
 }
 
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct RoyaltiesInfoResponse {
+    pub royalty_address: String,
+    // Note that this must be the same denom as that passed in to RoyaltyInfo
+    // rounding up or down is at the discretion of the implementer
+    pub royalty_amount: Uint128,
+}
+
+/// Shows if the contract implements royalties
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct CheckRoyaltiesResponse {
+    pub royalty_payments: bool,
+}
--- contracts/nft-station-minter/src/state.rs
+++ contracts/nft-station-minter/src/state.rs
@@ -5,15 +5,6 @@ use cosmwasm_std::Addr;
 use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
-}
-
-
-pub const STATE: Item<State> = Item::new("state");
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
     pub owner: Addr,
     pub base_token_uri: String,
@@ -28,9 +19,7 @@ pub struct Config {
     pub royalty_payment_address: Option<String>,
 }
 
-
 pub const CONFIG: Item<Config> = Item::new("config");
 pub const MINTABLE_TOKEN_IDS: Map<u32, bool> = Map::new("mintable_ids");
 pub const MINTABLE_NUM_TOKENS: Item<u32> = Item::new("mintable_num_tokens");
 pub const CW721_ADDRESS: Item<Addr> = Item::new("cw721_address");
-
